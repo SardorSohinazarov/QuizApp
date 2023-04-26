5fff62b8cff52840870f575081ecbfbda5259b86
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-04-26 15:55:13 +0500

    Quiz App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
